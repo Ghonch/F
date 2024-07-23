6e278b418c90ce26fffbddef53b7553fea27cc54
Ghonch <mahlabamuntgomry@gmail.com>
2024-07-23 22:59:16 +0200

    Add files via upload

---
+++ TestingProject.java
@@ -0,0 +1,10 @@
+
+public class TestingProject {
+
+    public static void main(String[] args) {
+        
+  System.out.println("Hello World");
+           
+        }
+                }           
+    
